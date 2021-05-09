7f2b311ba985e774629846fb84f1837442dbfa89
Vitoria Helen <vitoriahellen3004@gmail.com>
2021-05-09 15:43:54 -0300

    nomecompletoJavaS

---
+++ index.js
@@ -0,0 +1,7 @@
+
+let primeironome = prompt("Digite seu primeiro nome:");
+
+let segundonome = prompt("Digite seu sobrenome:"); 
+
+alert("Nome completo: " + primeironome + " " + segundonome); 
+alert("Nome catálogo: " + segundonome.toUpperCase() + "," + " " + primeironome);
